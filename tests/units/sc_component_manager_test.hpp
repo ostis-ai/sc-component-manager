f51fd53078729ef4ff86d5b9f02a6df625a25dbf
MksmOrlov <tawnhall8.5@gmail.com>
2023-02-13 19:45:48 +0300

    [test] Remove repos parser

--- tests/units/sc_component_manager_test.hpp
+++ tests/units/sc_component_manager_test.hpp
@@ -10,7 +10,6 @@
 
 #include "sc-memory/sc_memory.hpp"
 
-#include "src/manager/commands/command_init/repos_parser/repos_parser.hpp"
 #include "src/manager/command_parser/sc_component_manager_command_parser.hpp"
 
 #include <memory>
@@ -21,7 +20,6 @@ protected:
   void SetUp() override
   {
     m_commandParser = std::make_unique<ScComponentManagerParser>();
-    m_reposParser = std::make_unique<ReposParser>();
   }
 
   void TearDown() override
@@ -30,5 +28,4 @@ protected:
 
 protected:
   std::unique_ptr<ScComponentManagerParser> m_commandParser;
-  std::unique_ptr<ReposParser> m_reposParser;
 };

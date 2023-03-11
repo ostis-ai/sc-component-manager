9413b2127069ec5e72fb88d5266c40179e7ebac8
SHtr3s <stas.butrin1331@gmail.com>
2023-03-11 14:52:33 +0300

    [utils] Add get specification address util

--- src/manager/utils/sc_component_utils.cpp
+++ src/manager/utils/sc_component_utils.cpp
@@ -8,6 +8,7 @@
 #include <sc-memory/sc_type.hpp>
 #include <sc-memory/sc_iterator.hpp>
 #include <sc-agents-common/utils/IteratorUtils.hpp>
+#include <sc-agents-common/utils/CommonUtils.hpp>
 #include "src/manager/commands/keynodes/ScComponentManagerKeynodes.hpp"
 #include "sc_component_utils.hpp"
 
@@ -93,5 +94,54 @@ ScAddr SearchUtils::GetComponentInstallationMethod(ScMemoryContext * context, Sc
 
   return componentInstallationMethod;
 }
+
+/**
+ * @brief Get vector of ScLinks with specification address.
+ * @param context current sc-memory context
+ * @param componentSpecificationAddr sc-addr of specification node
+ * @return Vector of sc-addr for sc-links which contain url address,
+ * return empty vector if no links found
+*/
+ScAddrVector SearchUtils::GetSpecificationAddress(
+    ScMemoryContext * context,
+    ScAddr const & componentSpecificationAddr)
+{
+  ScIterator5Ptr alternativeAddressesSetIterator = context->Iterator5(
+      componentSpecificationAddr,
+      ScType::EdgeAccessConstPosPerm,
+      ScType::NodeTuple,
+      ScType::EdgeAccessConstPosPerm,
+      keynodes::ScComponentManagerKeynodes::nrel_alternative_addresses);
+  if (!alternativeAddressesSetIterator->Next())
+  {
+    SC_LOG_ERROR("No alternative addresses set found");
+    return {};
+  }
+  ScAddr const & alternativeAddressesSet = alternativeAddressesSetIterator->Get(2);
+
+  if (utils::CommonUtils::isEmpty(context, alternativeAddressesSet))
+  {
+    SC_LOG_ERROR("Alternative addresses set is empty");
+    return {};
+  }
+
+  ScAddr specificationAddressAddr = utils::IteratorUtils::getFirstFromSet(context, alternativeAddressesSet, true);
+
+  if (!specificationAddressAddr.IsValid())
+  {
+    specificationAddressAddr = utils::IteratorUtils::getAnyFromSet(context, alternativeAddressesSet);
+  }
+
+  ScAddrVector specificationAddressLinks =
+      utils::IteratorUtils::getAllWithType(context, specificationAddressAddr, ScType::LinkConst);
+
+  if (specificationAddressLinks.empty())
+  {
+    SC_LOG_ERROR("No sc-links connected with address node");
+    return {};
+  }
+
+  return specificationAddressLinks;
+};
 
 }  // namespace componentUtils

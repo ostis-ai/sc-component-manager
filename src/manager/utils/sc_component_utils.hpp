cc44cb2ca8d30512db4d5e47e5e3124ea2271976
Aleksandra Efimova <sashaefimova007@gmail.com>
2023-03-27 01:31:03 +0300

    [utils] Add install utils

--- src/manager/utils/sc_component_utils.hpp
+++ src/manager/utils/sc_component_utils.hpp
@@ -8,27 +8,42 @@
 
 #include <sc-memory/sc_memory.hpp>
 
-namespace componentUtils
-{
+namespace componentUtils {
 
-class SearchUtils
-{
-public:
-  static ScAddr GetComponentAddress(ScMemoryContext * context, ScAddr const & componentAddr);
+    class SearchUtils
+    {
+    public:
+        static ScAddr GetComponentAddress(ScMemoryContext *context, ScAddr const &componentAddr);
 
-  static ScAddrVector GetComponentDependencies(ScMemoryContext * context, ScAddr const & componentAddr);
+        static ScAddrVector GetComponentDependencies(ScMemoryContext *context, ScAddr const &componentAddr);
 
-  static ScAddr GetComponentInstallationMethod(ScMemoryContext * context, ScAddr const & componentAddr);
+        static ScAddr GetComponentInstallationMethod(ScMemoryContext *context, ScAddr const &componentAddr);
 
-  static ScAddrVector GetSpecificationAddress(ScMemoryContext * context, ScAddr const & componentSpecificationAddr);
+        static ScAddrVector GetSpecificationAddress(ScMemoryContext *context, ScAddr const &componentSpecificationAddr);
 
-  static ScAddr GetRepositoryAddress(ScMemoryContext * context, ScAddr const & repositoryAddr);
-};
+        static ScAddr GetRepositoryAddress(ScMemoryContext *context, ScAddr const &repositoryAddr);
+    };
 
-class LoadUtils
-{
-public:
-  static void LoadScsFilesInDir(ScMemoryContext * context, std::string const & dirPath);
-};
+    class InstallUtils
+    {
+    public:
+        static bool IsReusable(ScMemoryContext *context, ScAddr const &componentAddr);
+
+        static bool IsComponentInstallationMethodValid(ScMemoryContext *context, ScAddr const &componentAddr);
+
+        static std::string GetComponentAddressStr(ScMemoryContext *context, ScAddr const &componentAddr);
+
+        static std::string GetComponentDirName(ScMemoryContext *context, ScAddr const &componentAddr,
+                                               const std::string &specificationsPath);
+
+        static std::vector <std::string> GetInstallScripts(ScMemoryContext *context, ScAddr const &componentAddr);
+
+    };
+
+    class LoadUtils
+    {
+    public:
+        static void LoadScsFilesInDir(ScMemoryContext *context, std::string const &dirPath);
+    };
 
 }  // namespace componentUtils

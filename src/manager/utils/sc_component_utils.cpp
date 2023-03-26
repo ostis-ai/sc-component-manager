896198fc21cbc1025ff2c0bfd5e86cedafda94a1
Aleksandra Efimova <sashaefimova007@gmail.com>
2023-03-27 01:31:03 +0300

    [utils] Add install utils

--- src/manager/utils/sc_component_utils.cpp
+++ src/manager/utils/sc_component_utils.cpp
@@ -16,8 +16,7 @@
 #include "src/manager/commands/keynodes/ScComponentManagerKeynodes.hpp"
 #include "sc_component_utils.hpp"
 
-namespace componentUtils
-{
+namespace componentUtils {
 
 /**
  * @brief Get content of component address link content
@@ -25,23 +24,23 @@ namespace componentUtils
  * @param componentAddr sc-addr of the component to install
  * @return sc-addr of component address
  */
-ScAddr SearchUtils::GetComponentAddress(ScMemoryContext * context, ScAddr const & componentAddr)
-{
-  ScAddr componentAddressAddr;
-  ScIterator5Ptr const & componentAddressIterator = context->Iterator5(
-      componentAddr,
-      ScType::EdgeDCommonConst,
-      ScType::LinkConst,
-      ScType::EdgeAccessConstPosPerm,
-      keynodes::ScComponentManagerKeynodes::nrel_component_address);
+    ScAddr SearchUtils::GetComponentAddress(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        ScAddr componentAddressAddr;
+        ScIterator5Ptr const &componentAddressIterator = context->Iterator5(
+                componentAddr,
+                ScType::EdgeDCommonConst,
+                ScType::LinkConst,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_component_address);
 
-  if (componentAddressIterator->Next())
-  {
-    componentAddressAddr = componentAddressIterator->Get(2);
-  }
+        if (componentAddressIterator->Next())
+        {
+            componentAddressAddr = componentAddressIterator->Get(2);
+        }
 
-  return componentAddressAddr;
-}
+        return componentAddressAddr;
+    }
 
 /**
  * @brief Get component dependencies vector
@@ -50,30 +49,31 @@ ScAddr SearchUtils::GetComponentAddress(ScMemoryContext * context, ScAddr const 
  * @return ScAddrVector consists of component dependencies sc-addrs,
  * return empty vector if component has no dependencies
  */
-ScAddrVector SearchUtils::GetComponentDependencies(ScMemoryContext * context, ScAddr const & componentAddr)
-{
-  ScAddrVector componentDependencies;
-  ScAddrVector componentCurrentDependencies;
-  ScAddr componentDependenciesSet;
+    ScAddrVector SearchUtils::GetComponentDependencies(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        ScAddrVector componentDependencies;
+        ScAddrVector componentCurrentDependencies;
+        ScAddr componentDependenciesSet;
 
-  ScIterator5Ptr const & componentDependenciesIterator = context->Iterator5(
-      componentAddr,
-      ScType::EdgeDCommonConst,
-      ScType::NodeConst,
-      ScType::EdgeAccessConstPosPerm,
-      keynodes::ScComponentManagerKeynodes::nrel_component_dependencies);
+        ScIterator5Ptr const &componentDependenciesIterator = context->Iterator5(
+                componentAddr,
+                ScType::EdgeDCommonConst,
+                ScType::NodeConst,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_component_dependencies);
 
-  while (componentDependenciesIterator->Next())
-  {
-    componentDependenciesSet = componentDependenciesIterator->Get(2);
-    componentCurrentDependencies =
-        utils::IteratorUtils::getAllWithType(context, componentDependenciesSet, ScType::NodeConst);
-    componentDependencies.insert(
-        componentDependencies.cend(), componentCurrentDependencies.cbegin(), componentCurrentDependencies.cend());
-  }
+        while (componentDependenciesIterator->Next())
+        {
+            componentDependenciesSet = componentDependenciesIterator->Get(2);
+            componentCurrentDependencies =
+                    utils::IteratorUtils::getAllWithType(context, componentDependenciesSet, ScType::NodeConst);
+            componentDependencies.insert(
+                    componentDependencies.cend(), componentCurrentDependencies.cbegin(),
+                    componentCurrentDependencies.cend());
+        }
 
-  return componentDependencies;
-}
+        return componentDependencies;
+    }
 
 /**
  * @brief Get component installation method
@@ -81,23 +81,23 @@ ScAddrVector SearchUtils::GetComponentDependencies(ScMemoryContext * context, Sc
  * @param componentAddr sc-addr of the component to install
  * @return sc-addr of the component installation method
  */
-ScAddr SearchUtils::GetComponentInstallationMethod(ScMemoryContext * context, ScAddr const & componentAddr)
-{
-  ScAddr componentInstallationMethod;
-  ScIterator5Ptr const & componentDependenciesIterator = context->Iterator5(
-      componentAddr,
-      ScType::EdgeDCommonConst,
-      ScType::NodeConst,
-      ScType::EdgeAccessConstPosPerm,
-      keynodes::ScComponentManagerKeynodes::nrel_installation_method);
+    ScAddr SearchUtils::GetComponentInstallationMethod(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        ScAddr componentInstallationMethod;
+        ScIterator5Ptr const &componentDependenciesIterator = context->Iterator5(
+                componentAddr,
+                ScType::EdgeDCommonConst,
+                ScType::NodeConst,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_installation_method);
 
-  if (componentDependenciesIterator->Next())
-  {
-    componentInstallationMethod = componentDependenciesIterator->Get(2);
-  }
+        if (componentDependenciesIterator->Next())
+        {
+            componentInstallationMethod = componentDependenciesIterator->Get(2);
+        }
 
-  return componentInstallationMethod;
-}
+        return componentInstallationMethod;
+    }
 
 /**
  * @brief Get vector of ScLinks with specification address.
@@ -106,46 +106,47 @@ ScAddr SearchUtils::GetComponentInstallationMethod(ScMemoryContext * context, Sc
  * @return Vector of sc-addr for sc-links which contain url address,
  * return empty vector if no links found
  */
-ScAddrVector SearchUtils::GetSpecificationAddress(ScMemoryContext * context, ScAddr const & componentSpecificationAddr)
-{
-  ScAddrVector specificationAddressLinks;
+    ScAddrVector
+    SearchUtils::GetSpecificationAddress(ScMemoryContext *context, ScAddr const &componentSpecificationAddr)
+    {
+        ScAddrVector specificationAddressLinks;
 
-  ScIterator5Ptr const & alternativeAddressesSetIterator = context->Iterator5(
-      componentSpecificationAddr,
-      ScType::EdgeDCommonConst,
-      ScType::NodeTuple,
-      ScType::EdgeAccessConstPosPerm,
-      keynodes::ScComponentManagerKeynodes::nrel_alternative_addresses);
+        ScIterator5Ptr const &alternativeAddressesSetIterator = context->Iterator5(
+                componentSpecificationAddr,
+                ScType::EdgeDCommonConst,
+                ScType::NodeTuple,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_alternative_addresses);
 
-  if (!alternativeAddressesSetIterator->Next())
-  {
-    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No alternative addresses set found");
-  }
+        if (!alternativeAddressesSetIterator->Next())
+        {
+            SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No alternative addresses set found");
+        }
 
-  ScAddr const & alternativeAddressesSet = alternativeAddressesSetIterator->Get(2);
+        ScAddr const &alternativeAddressesSet = alternativeAddressesSetIterator->Get(2);
 
-  if (utils::CommonUtils::isEmpty(context, alternativeAddressesSet))
-  {
-    SC_THROW_EXCEPTION(utils::ExceptionAssert, "Alternative addresses set is empty");
-  }
+        if (utils::CommonUtils::isEmpty(context, alternativeAddressesSet))
+        {
+            SC_THROW_EXCEPTION(utils::ExceptionAssert, "Alternative addresses set is empty");
+        }
 
-  ScAddr specificationAddressAddr = utils::IteratorUtils::getFirstFromSet(context, alternativeAddressesSet, true);
+        ScAddr specificationAddressAddr = utils::IteratorUtils::getFirstFromSet(context, alternativeAddressesSet, true);
 
-  if (!specificationAddressAddr.IsValid())
-  {
-    specificationAddressAddr = utils::IteratorUtils::getAnyFromSet(context, alternativeAddressesSet);
-  }
+        if (!specificationAddressAddr.IsValid())
+        {
+            specificationAddressAddr = utils::IteratorUtils::getAnyFromSet(context, alternativeAddressesSet);
+        }
 
-  specificationAddressLinks =
-      utils::IteratorUtils::getAllWithType(context, specificationAddressAddr, ScType::LinkConst);
+        specificationAddressLinks =
+                utils::IteratorUtils::getAllWithType(context, specificationAddressAddr, ScType::LinkConst);
 
-  if (specificationAddressLinks.empty())
-  {
-    SC_THROW_EXCEPTION(utils::ExceptionAssert, "No sc-links connected with address node");
-  }
+        if (specificationAddressLinks.empty())
+        {
+            SC_THROW_EXCEPTION(utils::ExceptionAssert, "No sc-links connected with address node");
+        }
 
-  return specificationAddressLinks;
-};
+        return specificationAddressLinks;
+    };
 
 /**
  * @brief Get sc-addr of sc-link with repository address.
@@ -153,54 +154,156 @@ ScAddrVector SearchUtils::GetSpecificationAddress(ScMemoryContext * context, ScA
  * @param componentSpecificationAddr sc-addr of repository node
  * @return Sc-addr of link that contains url address of repository.
  */
-ScAddr SearchUtils::GetRepositoryAddress(ScMemoryContext * context, ScAddr const & repositoryAddr)
-{
-  ScAddr addressLinkAddr;
-
-  ScIterator5Ptr const & repositoryAddressIterator = context->Iterator5(
-      repositoryAddr,
-      ScType::EdgeDCommonConst,
-      ScType::NodeConst,
-      ScType::EdgeAccessConstPosPerm,
-      keynodes::ScComponentManagerKeynodes::nrel_repository_address);
-
-  if (!repositoryAddressIterator->Next())
-  {
-    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No address found for repository");
-  }
-
-  ScAddr const & repositoryAddressAddr = repositoryAddressIterator->Get(2);
-
-  ScIterator3Ptr const & addressLinkIterator =
-      context->Iterator3(repositoryAddressAddr, ScType::EdgeAccessConstPosPerm, ScType::LinkConst);
-
-  if (!addressLinkIterator->Next())
-  {
-    SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No links for repository address found");
-  }
-
-  addressLinkAddr = addressLinkIterator->Get(2);
-
-  return addressLinkAddr;
-};
-
-void LoadUtils::LoadScsFilesInDir(ScMemoryContext * context, std::string const & dirPath)
-{
-  ScsLoader loader;
-  DIR * dir;
-  struct dirent * diread;
-  if ((dir = opendir(dirPath.c_str())) != nullptr)
-  {
-    while ((diread = readdir(dir)) != nullptr)
+    ScAddr SearchUtils::GetRepositoryAddress(ScMemoryContext *context, ScAddr const &repositoryAddr)
     {
-      std::string filename = diread->d_name;
-      if (filename.rfind(".scs") != std::string::npos)
-      {
-        loader.loadScsFile(*context, dirPath + "/" + filename);
-      }
+        ScAddr addressLinkAddr;
+
+        ScIterator5Ptr const &repositoryAddressIterator = context->Iterator5(
+                repositoryAddr,
+                ScType::EdgeDCommonConst,
+                ScType::NodeConst,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_repository_address);
+
+        if (!repositoryAddressIterator->Next())
+        {
+            SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No address found for repository");
+        }
+
+        ScAddr const &repositoryAddressAddr = repositoryAddressIterator->Get(2);
+
+        ScIterator3Ptr const &addressLinkIterator =
+                context->Iterator3(repositoryAddressAddr, ScType::EdgeAccessConstPosPerm, ScType::LinkConst);
+
+        if (!addressLinkIterator->Next())
+        {
+            SC_THROW_EXCEPTION(utils::ExceptionItemNotFound, "No links for repository address found");
+        }
+
+        addressLinkAddr = addressLinkIterator->Get(2);
+
+        return addressLinkAddr;
+    };
+
+    /**
+  *
+  * @param context
+  * @param componentAddr
+  * @return
+  */
+    bool InstallUtils::IsReusable(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        bool result = true;
+        ScIterator3Ptr const reusableComponentCLassIterator = context->Iterator3(
+                keynodes::ScComponentManagerKeynodes::concept_reusable_component, ScType::EdgeAccessConstPosPerm,
+                componentAddr);
+        if (!reusableComponentCLassIterator->Next())
+        {
+            SC_LOG_WARNING("Component is not a reusable component.");
+            result = false;
+        }
+        return result;
     }
-    closedir(dir);
-  }
-}
+
+    /**
+     *
+     * @param context
+     * @param componentAddr
+     * @return
+     */
+    std::vector <std::string> InstallUtils::GetInstallScripts(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        ScIterator5Ptr const &installScriptsIterator = context->Iterator5(
+                componentAddr,
+                ScType::EdgeDCommonConst,
+                ScType::LinkConst,
+                ScType::EdgeAccessConstPosPerm,
+                keynodes::ScComponentManagerKeynodes::nrel_install_scripts
+        );
+
+        //TODO: bug: in kb folder there are 2 specifications for one rep, so all links duplicated
+        std::string script;
+        while (installScriptsIterator->Next())
+        {
+            const ScAddr &scriptAddrs = installScriptsIterator->Get(2);
+            context->GetLinkContent(scriptAddrs, script);
+            SC_LOG_WARNING("ScComponentManager: Install script found:" + script);
+        }
+        return {};
+    }
+
+    /**
+     *
+     * @param context
+     * @param componentAddr
+     * @return
+     */
+    bool InstallUtils::IsComponentInstallationMethodValid(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        bool result = true;
+        ScAddr const &componentInstallationMethod =
+                componentUtils::SearchUtils::GetComponentInstallationMethod(context, componentAddr);
+        if (!componentInstallationMethod.IsValid())
+        {
+            SC_LOG_WARNING("Component installation method isn't valid.");
+            result = false;
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param context
+     * @param componentAddr
+     * @return
+     */
+    std::string InstallUtils::GetComponentAddressStr(ScMemoryContext *context, ScAddr const &componentAddr)
+    {
+        ScAddr const &componentAddressAddr = componentUtils::SearchUtils::GetComponentAddress(context, componentAddr);
+        std::string componentAddressContent;
+        context->GetLinkContent(componentAddressAddr, componentAddressContent);
+        return componentAddressContent;
+    }
+
+    /**
+     *
+     * @param context
+     * @param componentAddr
+     * @param specificationsPath
+     * @return
+     */
+    std::string InstallUtils::GetComponentDirName(ScMemoryContext *context, ScAddr const &componentAddr,
+                                                  const std::string &specificationsPath)
+    {
+        std::string const &componentAddressContent = componentUtils::InstallUtils::GetComponentAddressStr(context,
+                                                                                                          componentAddr);
+        size_t componentDirNameIndex = componentAddressContent.rfind('/');
+        std::string componentDirName = specificationsPath + componentAddressContent.substr(componentDirNameIndex);
+        return componentDirName;
+    }
+
+    /**
+     *
+     * @param context
+     * @param dirPath
+     */
+    void LoadUtils::LoadScsFilesInDir(ScMemoryContext *context, std::string const &dirPath)
+    {
+        ScsLoader loader;
+        DIR *dir;
+        struct dirent *diread;
+        if ((dir = opendir(dirPath.c_str())) != nullptr)
+        {
+            while ((diread = readdir(dir)) != nullptr)
+            {
+                std::string filename = diread->d_name;
+                if (filename.rfind(".scs") != std::string::npos)
+                {
+                    loader.loadScsFile(*context, dirPath + "/" + filename);
+                }
+            }
+            closedir(dir);
+        }
+    }
 
 }  // namespace componentUtils

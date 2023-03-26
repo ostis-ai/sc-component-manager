2d1823bd380999e2d208dcbb2a8ea677a78fd470
Aleksandra Efimova <sashaefimova007@gmail.com>
2023-03-27 01:32:16 +0300

    [keynodes] Update keynodes

--- src/manager/commands/keynodes/ScComponentManagerKeynodes.hpp
+++ src/manager/commands/keynodes/ScComponentManagerKeynodes.hpp
@@ -66,6 +66,9 @@ public:
 
   SC_PROPERTY(Keynode("nrel_repository_address"), ForceCreate(ScType::NodeConstNoRole))
   static ScAddr nrel_repository_address;
+
+  SC_PROPERTY(Keynode("nrel_install_scripts"), ForceCreate(ScType::NodeConstNoRole))
+  static ScAddr nrel_install_scripts;
 };
 
 }  // namespace keynodes

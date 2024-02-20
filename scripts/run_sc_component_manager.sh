631ec2c511e46920e2b1c2e82a67e3d355ff42a7
Anna Makarenko <manahafor4@gmail.com>
2024-02-20 12:06:17 +0300

    [scripts] Build only in sc-machine

--- scripts/run_sc_component_manager.sh
+++ scripts/run_sc_component_manager.sh
@@ -4,11 +4,7 @@ set -eo pipefail
 
 CURRENT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 source "${CURRENT_DIR}/formats.sh"
-
-if [[ -z "${BINARY_PATH}" ]];
-then
-  source "${CURRENT_DIR}/set_vars.sh"
-fi
+source "${CURRENT_DIR}/set_vars.sh"
 
 INTERACTIVE_MODE=""
 

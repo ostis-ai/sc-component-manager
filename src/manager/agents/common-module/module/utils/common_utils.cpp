c37399277bba5b04afb6528cb5ccf09b4fbe405c
Anna Makarenko <manahafor4@gmail.com>
2024-03-24 01:20:55 +0300

    [utils] Don't generate nodes with empty identificators

--- src/manager/agents/common-module/module/utils/common_utils.cpp
+++ src/manager/agents/common-module/module/utils/common_utils.cpp
@@ -92,7 +92,11 @@ ScAddrVector CommonUtils::GetNodesUnderParameter(
         m_memoryCtx.Iterator3(parameterNode, ScType::EdgeAccessConstPosPerm, ScType::NodeConst);
     while (componentsIterator->Next())
     {
-      components.push_back(componentsIterator->Get(2));
+      //we can't download "empty" nodes
+      if(!m_memoryCtx.HelperGetSystemIdtf(componentsIterator->Get(2)).empty())
+      {
+        components.push_back(componentsIterator->Get(2));
+      }
     }
   }
   return components;

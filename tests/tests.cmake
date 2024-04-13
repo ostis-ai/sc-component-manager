9e50202cca8fde9b986609e6ad5aa835e5f2212d
MksmOrlov <orlovmaksimkonstit@gmail.com>
2024-04-13 15:46:52 +0300

    [build] Fix cmakes

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,12 +1,18 @@
 SET(AGENTS_SRC "${CMAKE_CURRENT_LIST_DIR}/../src/manager/agents")
 SET(CONSOLE_SRC "${CMAKE_CURRENT_LIST_DIR}/../src/manager/console-interface")
 
-make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
-    NAME sc-component-manager-tests
-    DEPENDS sc-agents-common sc-memory sc-core common-lib search-lib install-lib init-lib
-    INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${GLIB2_LIBRARIES} ${AGENTS_SRC} ${CONSOLE_SRC}
-)
+file(GLOB_RECURSE SOURCES "${CMAKE_CURRENT_LIST_DIR}/units/*.cpp" "${CMAKE_CURRENT_LIST_DIR}/units/*.hpp")
+add_executable(sc-component-manager-test ${SOURCES})
 
+target_link_libraries(sc-component-manager-test sc-agents-common sc-memory sc-core common-lib search-lib install-lib init-lib gtest_main)
+
+target_include_directories(
+        sc-component-manager-test
+        PRIVATE ${SC_MEMORY_SRC}/tests/sc-memory/_test ${GLIB2_LIBRARIES} ${AGENTS_SRC} ${CONSOLE_SRC})
+
+set_target_properties(sc-component-manager-test PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${SC_BIN_PATH})
+
+add_test(NAME sc-component-manager-test COMMAND sc-component-manager-test)
 if(${SC_CLANG_FORMAT_CODE})
     target_clangformat_setup(sc-component-manager-tests)
 endif()
--- thirdparty/gtest.cmake
+++ thirdparty/gtest.cmake
@@ -1,12 +1,4 @@
-add_library(gtest STATIC
-  "googletest/googletest/src/gtest-all.cc"
-  "googletest/googlemock/src/gmock-all.cc"
-  "googletest/googletest/src/gtest_main.cc"
-)
-
-target_include_directories(gtest 
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/googletest/googletest
-  PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/googletest/googletest/include
-  PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/googletest/googlemock
-  PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/googletest/googlemock/include
-)
+if(NOT TARGET gtest)
+    set(BUILD_GMOCK OFF)
+    add_subdirectory(./googletest)
+endif()
